e9f73fc7447f64f8f0db571746adbe8834013a9d
Duncan <king29712@gmail.com>
2021-12-01 17:22:00 +0800

    data request

---
+++ test4-DataRequest/master_04/master_04.ino
@@ -0,0 +1,31 @@
+#include <Wire.h>
+
+#define SLAVE_ADDRESS 0x12
+#define SERIAL_BAUD 57600 
+#define DATA_SIZE 8
+
+void setup()
+{
+  Wire.begin();
+ 
+  Serial.begin(SERIAL_BAUD);
+  Serial.println("I2C Master.04 started");
+  Serial.println();
+}
+ 
+ 
+void loop()
+{
+  if (Serial.available()) {
+
+    Wire.beginTransmission(SLAVE_ADDRESS);
+    Wire.requestFrom(SLAVE_ADDRESS, DATA_SIZE);
+    if (Wire.available()) {
+      Serial.print("Data returned: ");
+      while (Wire.available()) Serial.print((char) Wire.read());
+      Serial.println();
+    }
+    Wire.endTransmission();
+    while(Serial.available()) Serial.read();  // Clear the serial buffer, in this example, just request data from slave
+  }
+}
