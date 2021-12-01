e2b42dda0c0e59aa9d8c70399a7b269f18cb94c5
Duncan <king29712@gmail.com>
2021-12-01 17:20:39 +0800

    data receive

---
+++ test2-DataReceive/slave_02/slave_02.ino
@@ -0,0 +1,24 @@
+#include <Wire.h>
+
+#define SLAVE_ADDRESS 0x12
+#define SERIAL_BAUD 57600 
+
+void setup() {
+  Wire.begin(SLAVE_ADDRESS);    // join I2C bus as a slave with address 1
+  Wire.onReceive(receiveEvent); // register event
+
+  Serial.begin(SERIAL_BAUD);
+  Serial.println("I2C Slave.02 started\n");
+}
+
+void loop() {
+}
+
+void receiveEvent(int count) {
+  Serial.println("Receive Data:");
+  while(Wire.available()) {
+    Serial.print((char) Wire.read());
+  }
+  Serial.println("\n");
+}
+
